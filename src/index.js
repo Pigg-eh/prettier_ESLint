351b2228468474222016d05881384f8fb7fd41bf
Ed Hunter <ep.hunter23@gmail.com>
2024-11-26 10:45:00 +0800

    copy over from webpack template

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import _ from 'lodash';
+import './style.css'
+
+function component() {
+  const element = document.createElement('div');
+
+  // Lodash, now imported by this script
+  // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = 'THE FAST test for commit'
+  element.classList.add('hello');
+
+  return element;
+}
+ 
+  
+document.body.appendChild(component());
